b71de34dcbebe2dc931d707cdb51f1b5be59d117
Ana Beatriz <anabeatrizbm042@gmail.com>
2026-09-21 14:12:34 -0300

    Implementando o Hello World multilinguas(simples)

--- hello.py
+++ hello.py
@@ -1,6 +1,38 @@
 #!/usr/bin/env python3
 
-print ('hello word')
+"""Hello Word multilinguas
+
+Programa exibe mensagem de acordo com a lingua configurada no ENV.
+
+Como usar:
+
+Tenha a variavel de ambiente LANG devidamente configurada. Ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Ana"
+__license__ = "Unlicense"
+
+import os
+
+
+msg = "Hello, World!"
+current_language = os.getenv("LANG", "en_US")[:5]
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+
+print (msg)
 
 
 
